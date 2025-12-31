b7027314b3ad04edff07a09c69ac4258ca1f1eae
jeremydidier <jeremy.didier86@gmail.com>
2025-12-31 09:18:15 -0500

    connection and deploying on aws  s3

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -1,8 +1,14 @@
 const core = require('@actions/core');
-const github = require('@actions/github');
+//const github = require('@actions/github');
 const exec = require('@actions/exec');
 function run() {
-    core.notice('Hello from my custom java Action!')
+    const bucketName = core.getInput('bucket-name',{required: true});
+    const bucketRegion = core.getInput('bucket-region', {required: true});
+    const distFolder = core.getInput('dist-folder', {required: true});
+
+    const s3Uri = `s3://${bucketName}`
+    exec.exec(`awx s3 sync ${distFolder} ${s3Uri} --region${bucketName}`);
+    core.notice('Hello from my custom java Action!');
 }
 
 run();
